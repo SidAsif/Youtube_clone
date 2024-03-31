0322d60d35b387c056d8b871f97c4ddccb7a8717
SidAsif <asif050md@gmail.com>
2024-03-31 22:37:15 +0530

    skeleton

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,21 @@
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, Skeleton } from "@mui/material";
 import PropTypes from "prop-types";
 import VideoCard from "./VideoCard";
 
-
 const Videos = ({ videos, drawerOpen, direction }) => {
   if (videos === null || videos === undefined) {
+    return (
+      <Stack direction={direction || "row"} gap={2}>
+        {[...Array(10)].map((_, idx) => (
+          <Box key={idx} width="100%">
+            <Skeleton variant="rectangular" width={330} height={180} />
+          </Box>
+        ))}
+      </Stack>
+    );
+  }
+
+  if (videos.length === 0) {
     return <div>No videos found.</div>;
   }
 
@@ -21,12 +32,12 @@ const Videos = ({ videos, drawerOpen, direction }) => {
           {item.id.videoId && (
             <VideoCard video={item} drawerOpen={drawerOpen} />
           )}
-        
         </Box>
       ))}
     </Stack>
   );
 };
+
 Videos.propTypes = {
   videos: PropTypes.arrayOf(PropTypes.object),
   drawerOpen: PropTypes.bool.isRequired,
